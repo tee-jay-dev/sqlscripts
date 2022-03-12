b33a6b0df58d23320400277a496ec0b4a002e6cf
Olatunji Ewebiyi <olatunji.ewebiyi@gmail.com>
2022-03-12 21:27:16 +0100

    Added new Column for customers orders count

--- getCustomers.sql
+++ getCustomers.sql
@@ -1,2 +1,7 @@
-SELECT [customer_id],[first_name],[last_name],[phone],[email]
-      ,[street],[city],[state],[zip_code] FROM [sales].[customers]
+SELECT c.customer_id as cud_id,first_name,last_name,phone,email
+      ,street,city,state,zip_code,isnull(count(o.order_id),0) as numOfOrders
+	  FROM sales.customers c
+	  join sales.orders o
+	  on c.customer_id = o.customer_id
+	  group by c.customer_id,first_name,last_name,phone,email
+      ,street,city,state,zip_code
